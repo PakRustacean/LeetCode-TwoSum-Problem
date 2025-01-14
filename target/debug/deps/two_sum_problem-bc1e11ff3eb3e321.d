52c27f9c338eeda66f274ba5c180fc797dd90482
Hassan Murtaza <hassanmurtaza.rustacean@gmail.com>
2025-01-15 03:01:42 +0500

    Delete target directory

--- target/debug/deps/two_sum_problem-bc1e11ff3eb3e321.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Moshlay\Documents\Rust\LeetCode-TwoSum-Problem\target\debug\deps\libtwo_sum_problem-bc1e11ff3eb3e321.rmeta: src\main.rs
-
-C:\Users\Moshlay\Documents\Rust\LeetCode-TwoSum-Problem\target\debug\deps\two_sum_problem-bc1e11ff3eb3e321.d: src\main.rs
-
-src\main.rs:
--- target/debug/deps/two_sum_problem.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Moshlay\Documents\Rust\LeetCode-TwoSum-Problem\target\debug\deps\two_sum_problem.exe: src\main.rs
-
-C:\Users\Moshlay\Documents\Rust\LeetCode-TwoSum-Problem\target\debug\deps\two_sum_problem.d: src\main.rs
-
-src\main.rs:
